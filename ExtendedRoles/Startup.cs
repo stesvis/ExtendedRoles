f7090631d8b5ff0df8b52cfdf7b5a3f04c2f1927
Cristian Merli <cristian@thirdstream.ca>
2017-08-17 09:08:52 -0600

    Add project files.

---
+++ ExtendedRoles/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExtendedRoles.Startup))]
+namespace ExtendedRoles
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
