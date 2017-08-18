814c9282556eadf3948ca1f20609824378734162
Cristian Merli <cristian@thirdstream.ca>
2017-08-18 14:33:46 -0600

    Uncommented ApplicationRole

--- ExtendedRoles/Models/ApplicationRole.cs
+++ ExtendedRoles/Models/ApplicationRole.cs
@@ -1,20 +1,28 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Microsoft.AspNet.Identity.EntityFramework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
 
-//namespace ExtendedRoles.Models
-//{
-//    public class ApplicationRole : IdentityRole
+namespace ExtendedRoles.Models
+{
+    public class ApplicationRole : IdentityRole
 
-// { public ApplicationRole() : base() { }
+    {
+        public ApplicationRole() : base()
+        {
+        }
 
-// public ApplicationRole(string name) : base(name) { }
+        public ApplicationRole(string name) : base(name)
+        {
+        }
 
-// public ApplicationRole(string name, int order) : base(name) { this.Order = order; }
+        public ApplicationRole(string name, int order) : base(name)
+        {
+            this.Order = order;
+        }
 
-//        public virtual int Order { get; set; }
-//    }
-//}
+        public virtual int Order { get; set; }
+    }
+}
